6136e28902904fd353ee16f75ae250f9b6effea2
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2023-11-19 23:01:35 -0500

    Sensor Class + Implement DHT22 Sensor

--- code.py
+++ code.py
@@ -1,15 +1,14 @@
-import microcontroller
+from sensors import Sensor
 import supervisor
 import time
 
 #disable autoreload, so board doesnt restart everytime a coding change is made
 supervisor.runtime.autoreload = False
 
-#gets cpu temperature instead of ambient temperature, not sure if its the most accurate but will probably do ¯\_(ツ)_/¯
-def get_temperature() -> float:
-    return microcontroller.cpu.temperature * (9/5) + 32
+#Declare sensor object
+sensor = Sensor()
 
 #mainloop
 while True:
-    time.sleep(0.1)
-    print(get_temperature())
+    time.sleep(1)
+    print(sensor.get_dict())
