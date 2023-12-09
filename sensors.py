aacccb0ad7635c44963a7ec96b3d9a3fa14021bb
WGrav <willemgrant85@gmail.com>
2023-12-08 21:36:32 -0500

    merge Weather Sensor class with Sensor class

--- sensors.py
+++ sensors.py
@@ -11,6 +11,14 @@ class Sensor:
     def __init__(self):
         self.dht22 = adafruit_dht.DHT22(GP16)
 
+        # define water sensor pins, GP15 and GP14 are currently placeholders and can will be changed once I get physical
+        # access to the hardware
+        self.wsensor_power = digitalio.DigitalInOut(board.GP15)
+        self.wsensor_power.direction = digitalio.Direction.OUTPUT
+        self.wsensor_signal = digitalio.DigitalInOut(board.GP14)
+        self.wsensor_signal.direction = digitalio.Direction.INPUT
+        # to read water sensor set self.wsensor_power = true and check self.wsensor_signal
+
     # returns dictionary of all sensor values
     def get_dict(self) -> dict:
         return {'temp': self.get_temperature(), 'humidity': self.get_humidity()}
@@ -22,14 +30,3 @@ class Sensor:
     # get humidity from DHT22 tied to GP16
     def get_humidity(self) -> float:
         return self.dht22.humidity
-
-
-class WaterSensor:
-    def __init__(self):
-        # define sensor pins, GP15 and GP14 are currently placeholders and can will be changed once I get physical
-        # access to the hardware
-        self.wsensor_power = digitalio.DigitalInOut(board.GP15)
-        self.wsensor_power.direction = digitalio.Direction.OUTPUT
-        self.wsensor_signal = digitalio.DigitalInOut(board.GP14)
-        self.wsensor_signal.direction = digitalio.Direction.INPUT
-        # to read water sensor set self.wsensor_power = true and check self.wsensor_signal
