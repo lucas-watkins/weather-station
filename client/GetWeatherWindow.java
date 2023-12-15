08db7e4435a08ee12ed09e5af83b3aa12e01aa0d
lucas-watkins <lucas-watkins@outlook.com>
2023-12-15 13:36:55 -0500

    Semi-working client gui

--- client/GetWeatherWindow.java
+++ client/GetWeatherWindow.java
@@ -20,14 +20,17 @@ public class GetWeatherWindow {
         //method chain go brr
         Image updateImage = new ImageIcon(this.getClass().getClassLoader().getResource("update.png")).getImage().getScaledInstance(64,64, Image.SCALE_SMOOTH);
 
+        vertBox.add(new JLabel("Updating Weather Information..."));
         vertBox.add(Box.createVerticalStrut(50));
         vertBox.add(okayBtn);
 
+        box.add(Box.createHorizontalStrut(10));
         box.add(new JLabel(new ImageIcon(updateImage)));
+        box.add(Box.createHorizontalStrut(10));
         box.add(vertBox);
 
         contentPane.add(box);
-        frame.setSize(250,100);
+        frame.setSize(350,200);
         frame.setVisible(true);
 
    }
